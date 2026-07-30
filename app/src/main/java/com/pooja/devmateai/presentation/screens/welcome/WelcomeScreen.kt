54df4bca6387fd6794d519b5d9c73aa2d50c7109
pooja singh <ps2137800@gmail.com>
2026-07-30 12:24:42 +0530

    created welcome screen ui

--- app/src/main/java/com/pooja/devmateai/presentation/screens/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/pooja/devmateai/presentation/screens/welcome/WelcomeScreen.kt
@@ -1,45 +1,67 @@
 package com.pooja.devmateai.presentation.screens.welcome
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.pooja.devmateai.R
 
 @Composable
 fun WelcomeScreen(
     onGetStartedClick: () -> Unit
 ) {
+
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
+        Icon(
+            painter = painterResource(id = R.drawable.ic_ai),
+            contentDescription = null,
+            modifier = Modifier.size(90.dp)
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
         Text(
             text = "DevMate AI",
-            style = MaterialTheme.typography.headlineLarge
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold
         )
 
         Spacer(modifier = Modifier.height(12.dp))
 
         Text(
-            text = "Your AI Interview Coach"
+            text = "Your Personal AI Interview Coach",
+            fontSize = 18.sp
         )
 
-        Spacer(modifier = Modifier.height(24.dp))
+        Spacer(modifier = Modifier.height(40.dp))
 
         Button(
-            onClick = onGetStartedClick
+            onClick = onGetStartedClick,
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp)
         ) {
-            Text(text = "Get Started")
+
+            Text(
+                text = "Get Started",
+                fontSize = 18.sp
+            )
+
         }
+
     }
 }
